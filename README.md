4d53802bc7d1d06166d322779edbc8e169e284ae
unknownexception <unknownliveid@hotmail.com>
2012-04-01 21:00:04 +0700

    add license

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-#[Preview Version] Totify
+#Totify! Текстовые фильтры
+
+Version: 0.1.1
 
 See [totify.com](http://totify.com)
+
+MIT License
+
--- totify.js/js/totify.util.js
+++ totify.js/js/totify.util.js
@@ -1,63 +1,66 @@
-﻿function log(message) {
-	//$('#debug').prepend(message + '<br/>');
-    console.log(message);
-}
+﻿// http://enterprisejquery.com/2010/10/how-good-c-habits-can-encourage-bad-javascript-habits-part-1/
+(function( util, $, undefined ) {
+    
+    util.log = function (message) {
+        console.log(message);
+    };
 
-function getCaret() {
-	var savedRange;
-    if(window.getSelection)//non IE Browsers
-    {
-        savedRange = window.getSelection();
-    }
-    else if(document.selection)//IE
-    { 
-        savedRange = document.selection.createRange();  
-    } 
-    return savedRange.focusOffset;
+    util.getCaret = function () {
+        var savedRange;
+        if(window.getSelection)//non IE Browsers
+        {
+            savedRange = window.getSelection();
+        }
+        else if(document.selection)//IE
+        { 
+            savedRange = document.selection.createRange();  
+        } 
+        return savedRange.focusOffset;
+    };
 
-}
-function setCaret(pos)
-{
-    ctrl = document.getElementById('editable');
-    ctrl.focus();
-    //pos = ctrl.value.length;   
-    log ("moving to pos = " + pos);
-    var range,selection;
-    if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
-    {
-        range = document.createRange();//Create a range (a range is a like the selection but invisible)
-        range.selectNodeContents(ctrl);//Select the entire contents of the element with the range
-        //range.focusOffset = pos;
-        //range.setStart(ctrl , pos-1);
-		//range.setEnd(ctrl , pos);
-        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
-        selection = window.getSelection();//get the selection object (allows you to change selection)
-        selection.removeAllRanges();//remove any selections already made
-        selection.addRange(range);//make the range you have just created the visible selection
-    }
-    else if(document.selection)//IE 8 and lower
-    { 
-        range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
-        range.moveToElementText(ctrl);//Select the entire contents of the element with the range
-        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
-        range.select();//Select the range (make it the visible selection
-    }
-}
+    util.setCaret = function (pos) {
+        ctrl = document.getElementById('editable');
+        ctrl.focus();
+        //pos = ctrl.value.length;   
+        log ("moving to pos = " + pos);
+        var range,selection;
+        if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+        {
+            range = document.createRange();//Create a range (a range is a like the selection but invisible)
+            range.selectNodeContents(ctrl);//Select the entire contents of the element with the range
+            //range.focusOffset = pos;
+            //range.setStart(ctrl , pos-1);
+            //range.setEnd(ctrl , pos);
+            range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+            selection = window.getSelection();//get the selection object (allows you to change selection)
+            selection.removeAllRanges();//remove any selections already made
+            selection.addRange(range);//make the range you have just created the visible selection
+        }
+        else if(document.selection)//IE 8 and lower
+        { 
+            range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+            range.moveToElementText(ctrl);//Select the entire contents of the element with the range
+            range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+            range.select();//Select the range (make it the visible selection
+        }
+    };
 
-function SaveDraft() {
-	log ('saving...' + $('#editable').html());
-	localStorage.setItem('contenteditable', $('#editable').html());
-}
+    util.saveDraft = function(id) {
+        log ('saving...' + $(id).html());
+        localStorage.setItem('contenteditable', $(id).html());
+    };
 
-function GetLastDraft() {
-	if (localStorage.getItem('contenteditable')) {
+    util.getLastDraft = function(id) {
+        if (localStorage.getItem('contenteditable')) 
+        {
             var content = localStorage.getItem('contenteditable');
             if (content === "")
-              $('#editable').html('<p></p>');
+              $(id).html('<p></p>');
             else
-		      $('#editable').html(localStorage.getItem('contenteditable'));
-		}
+              $(id).html(localStorage.getItem('contenteditable'));
+        }
         else
-            $('#editable').html("<p>Пришла пора писать красивее. Используйте всплывающие окна, включайте и выключайте фильтры</p>");
-
-}
+            $(id).html("<p>Пришла пора писать красивее. Используйте всплывающие окна, включайте и выключайте фильтры</p>");
+    };    
+    
+}(window.util = window.util || {}, jQuery));
