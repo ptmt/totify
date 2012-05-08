871cb33b7a81993b938ebbd18354e2c758e6424c
unknownexception <unknownliveid@hotmail.com>
2012-05-08 23:51:30 +0700

    nlp utils pos tagger .1

--- totify.filters/Master.fs
+++ totify.filters/Master.fs
@@ -8,14 +8,18 @@ open Totify.Protocol
 let prepareText text =     
     let r = TinyNLP.Tokenizer.tokenize text
     match r with
-        | None -> [ {Id = 1; Content = "prepareText error"; Class = TokenClass.Other} ]
-        | _ -> r.Value |> List.mapi (fun i x-> { Id = i; Content = x.Trim(); Class = TinyNLP.Tokenizer.tokenClassifier x})
+        | None ->             
+            [ {Id = 1; Content = "prepareText error"; Class = TokenClass.Other; Tag = ["UNKN"]} ]
+        | _ -> 
+            let t = Totify.NLP.tagTokens (["<S>"] @ r.Value @ ["</S"])
+            r.Value |> List.mapi (fun i x-> { Id = i; Content = x.Trim(); Class = TinyNLP.Tokenizer.tokenClassifier x; Tag = t.[i]})
 
 
 // FILTER #1: Synonymize It!
 let synonymFilter tokensList = 
     tokensList |> List.map (fun x -> { Token = x; Changes = [{ Variants = TinyNLP.Synonymizer.getSynonyms(TinyNLP.Stemming.Stem x.Content)} ]})
 
-
+//let tagger (nodeList: Node list) = 
+//    nodeList |> List.map (fun x -> { Token = x; Changes = x.Changes @ 
 let totify text =
     prepareText text |> synonymFilter
--- totify.filters/Protocol.fs
+++ totify.filters/Protocol.fs
@@ -6,6 +6,7 @@ type Token = {
     Id: int;
     Content: string;
     Class:TinyNLP.Tokenizer.TokenClass;
+    Tag:string list;
 }
 
 type ChangeType = 
