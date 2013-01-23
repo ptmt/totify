f492bbe792583232354a04139aa0086b613c3234
unknownexception <unknownliveid@hotmail.com>
2013-01-23 23:21:54 +0700

    reorganize directories

---
+++ src/app/webdeploy/Program.fs
@@ -0,0 +1,17 @@
+﻿module Totify.Web.Handler
+
+
+open Mario.HttpContext
+open Mario.WebServer
+
+
+
+let myHandler (req:HttpRequest) : HttpResponse =
+    match req.Uri with 
+        | "/test.fs" -> 
+            
+            { Json = Totify.WebDeploy.test_build  }     
+        | _ -> Mario.HttpUtility.badRequest
+    
+
+Mario.Start(myHandler, 8100)
